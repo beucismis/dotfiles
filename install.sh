2d66d54db1fc9689381c99a900835f611f5b95b6
beucismis <beucismis@tutamail.com>
2021-08-02 20:50:47 +0300

    Update install.sh

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if ! [ -x "$(command -v stow)" ]; then
+  echo "Error: stow is not installed." >&2
+  exit 1
+fi
+
+#
+
+cd ~
+git clone https://github.com/beucismis/dotfiles
+mv dotfiles .dotfiles
+
+#
+
+stow --adopt bash
+
+stow --adopt x11
+stow --adopt openbox
+stow --adopt tint2
+stow --adopt dunst
+
+stow --adopt gtk-3.0
+stow --adopt gtk-2.0
+stow --adopt qt5ct
+
+stow --adopt git
+stow --adopt feh
+stow --adopt terminator
+
+echo "Done." >&2
