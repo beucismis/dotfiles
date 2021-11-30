9125019a8d114a9b646b11ca8dabea6462f2d7b4
beucismis <beucismis@tutamail.com>
2021-11-30 11:45:12 +0300

    Add select-screenshot.sh

---
+++ bin/.local/bin/select-screenshot.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Allow screenshot to be selected from cursor, then display shot image.
+
+SCREENSHOT_DIR=$HOME/screenshots
+mkdir -p $SCREENSHOT_DIR
+sleep 0.2;
+scrot -s "$SCREENSHOT_DIR/%Y-%m-%d-%H%M%S_\$wx\$h.png" -e "xdg-open \$f"
+notify-send "New screenshot!"
