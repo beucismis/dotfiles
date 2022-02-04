bb4420c16e792773054f68d5ec1514ca52bdbf05
beucismis <beucismis@tutamail.com>
2022-02-04 15:16:21 +0300

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -5,7 +5,10 @@ if [[ "$(id -u)" != 0 ]]; then
   exit 1
 fi
 
-apt update && apt upgrade
+apt update && apt upgrade -y
+
+# Display Manager
+apt install -qq lightdm -y
 
 # Window Manager
 apt install -qq openbox -y
@@ -14,7 +17,7 @@ apt install -qq openbox-menu dmenu -y # Menu
 # Panel
 apt install -qq tint2 -y
 apt install -qq blueman -y # Bluetooth applet
-apt install -qq volumeicon-alsa -y # Volume control applet
+apt install -qq pasystray -y # Volume control applet
 apt install -qq network-manager-gnome -y # Network control applet
 
 # Deamons
@@ -24,6 +27,10 @@ apt install -qq dunst # Notification
 apt install -qq adwaita-icon-theme-full -y # GTK theme
 apt install -qq papirus-icon-theme -y # Icon theme
 apt install -qq comixcursors-righthanded -y # Cursor theme
+
+## CLI Tools
+apt install -qq htop -y # System monitor
+apt install -qq neofect -y # System fetcher
 
 ## General Tools
 apt install -qq terminator -y # Terminal emulator
@@ -38,12 +45,21 @@ apt install -qq xarchiver -y # Archive manager
 apt install -qq gparted bleachbit -y # Disk manager
 apt install -qq keepassxc -y # Password manager
 apt install -qq qbittorrent -y # Torrent client
+apt install -qq flameshot -y # Screenshot tool
+
+# Online Messaging
+cd /opt
+wget https://github.com/telegramdesktop/tdesktop/releases/download/v3.5.0/tsetup.3.5.0.tar.xz
+tar xjf tsetup.*.tar.xz
+rm tsetup.*.tar.xz
 
 # Internet Browser
 cd /opt
 wget https://ftp.mozilla.org/pub/firefox/releases/95.0/linux-x86_64/tr/firefox-95.0.tar.bz2
 tar xjf firefox-*.tar.bz2
 rm firefox-*.tar.bz2
+
+cd ~
 
 # Game Library
 dpkg --add-architecture i386
@@ -54,10 +70,10 @@ apt install -qq steam -y
 apt install -qq mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386 -y
 
 ## Config Tools
-apt install -qq lxappearance qt5ct -y # Theme config
+apt install -qq lxappearance qt5ct -y # Theme settings
 apt install -qq grub-customizer -y # Custom GRUB
 apt install -qq pavucontrol -y # Sound control
+apt install -qq lightdm-gtk-greeter-settings -y # LightDM settingsn -y # VPN too
 
-## CLI Tools
-apt install -qq htop -y # System monitor
-apt install -qq neofect -y # System fetcher
+## Others
+apt install -qq openvpn -y # VPN tool
