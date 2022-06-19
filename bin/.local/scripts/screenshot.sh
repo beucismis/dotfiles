33f0766ec9b66417417222145138fb96d960dab6
beucismis <beucismis@tutamail.com>
2022-06-19 22:37:15 +0300

    Remove some files

--- bin/.local/scripts/screenshot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Take screenshot of entire screen (multi-head) and display shot.
-
-SCREENSHOT_DIR=$HOME/Screenshots
-mkdir -p $SCREENSHOT_DIR
-sleep 0.2
-scrot -m "$SCREENSHOT_DIR/%Y-%m-%d-%H%M%S_\$wx\$h.png"
-notify-send "New screenshot!"
--- bin/.local/scripts/select-screenshot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Allow screenshot to be selected from cursor, then display shot image.
-
-SCREENSHOT_DIR=$HOME/screenshots
-mkdir -p $SCREENSHOT_DIR
-sleep 0.2
-scrot -s "$SCREENSHOT_DIR/%Y-%m-%d-%H%M%S_\$wx\$h.png"
-notify-send "New screenshot!"
