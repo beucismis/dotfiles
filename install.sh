5e6aac3cd64bb3371e3af84ac99fc7f30eb7385e
beucismis <beucismis@tutamail.com>
2022-07-05 20:46:18 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
-if ! [ -x "$(command -v stow)" ]; then
-  echo "Error: stow is not installed." >&2
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root!"
   exit 1
 fi
 
-#cd ~
-#git clone https://github.com/beucismis/dotfiles
-#mv dotfiles .dotfiles
+echo "Installing dependencies..."
+
+apt update && apt upgrde -y
+apt install git stow -y
+
+cd $HOME
+git clone https://github.com/beucismis/dotfiles
+mv dotfiles .dotfiles
+
+echo "Dotfiles are symlinked..."
 
 stow --adopt bash
-stow --adopt x11
-stow --adopt openbox
-stow --adopt tint2
-stow --adopt dunst
-stow --adopt gtk-3.0
-stow --adopt gtk-2.0
-stow --adopt qt5ct
+stow --adopt foot
 stow --adopt git
+stow --adopt mako
+stow --adopt sway
+stow --adopt pip
+stow --adopt waybar
+stow --adopt wofi
+stow --adopt etc -t /etc
+stow --adopt usr -t /usr
 
-stow etc -t /etc/
-
-stow --adopt bin
-stow --adopt images
-
-echo "Done." >&2
+echo "Done."
