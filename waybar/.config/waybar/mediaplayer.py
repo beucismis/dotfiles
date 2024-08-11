b9429e2eff2a9fd88c29bec92069c76f561413ed
beucismis <beucismis@tutamail.com>
2024-08-11 15:56:35 +0300

    Update mediaplayer.py

--- waybar/.config/waybar/mediaplayer.py
+++ waybar/.config/waybar/mediaplayer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import gi
+
 gi.require_version("Playerctl", "2.0")
 from gi.repository import Playerctl, GLib
 from gi.repository.Playerctl import Player
@@ -14,6 +15,7 @@ from typing import List
 
 logger = logging.getLogger(__name__)
 
+
 def signal_handler(sig, frame):
     logger.info("Received signal to stop, exiting")
     sys.stdout.write("\n")
@@ -27,15 +29,17 @@ class PlayerManager:
         self.manager = Playerctl.PlayerManager()
         self.loop = GLib.MainLoop()
         self.manager.connect(
-            "name-appeared", lambda *args: self.on_player_appeared(*args))
+            "name-appeared", lambda *args: self.on_player_appeared(*args)
+        )
         self.manager.connect(
-            "player-vanished", lambda *args: self.on_player_vanished(*args))
+            "player-vanished", lambda *args: self.on_player_vanished(*args)
+        )
 
         signal.signal(signal.SIGINT, signal_handler)
         signal.signal(signal.SIGTERM, signal_handler)
         signal.signal(signal.SIGPIPE, signal.SIG_DFL)
         self.selected_player = selected_player
-        self.excluded_player = excluded_player.split(',') if excluded_player else []
+        self.excluded_player = excluded_player.split(",") if excluded_player else []
 
         self.init_players()
 
@@ -55,8 +59,7 @@ class PlayerManager:
     def init_player(self, player):
         logger.info(f"Initialize new player: {player.name}")
         player = Playerctl.Player.new_from_name(player)
-        player.connect("playback-status",
-                       self.on_playback_status_changed, None)
+        player.connect("playback-status", self.on_playback_status_changed, None)
         player.connect("metadata", self.on_metadata_changed, None)
         self.manager.manage_player(player)
         self.on_metadata_changed(player, player.props.metadata)
@@ -67,9 +70,11 @@ class PlayerManager:
     def write_output(self, text, player):
         logger.debug(f"Writing output: {text}")
 
-        output = {"text": text,
-                  "class": "custom-" + player.props.player_name,
-                  "alt": player.props.player_name}
+        output = {
+            "text": text,
+            "class": "custom-" + player.props.player_name,
+            "alt": player.props.player_name,
+        }
 
         sys.stdout.write(json.dumps(output) + "\n")
         sys.stdout.flush()
@@ -79,7 +84,9 @@ class PlayerManager:
         sys.stdout.flush()
 
     def on_playback_status_changed(self, player, status, _=None):
-        logger.debug(f"Playback status changed for player {player.props.player_name}: {status}")
+        logger.debug(
+            f"Playback status changed for player {player.props.player_name}: {status}"
+        )
         self.on_metadata_changed(player, player.props.metadata)
 
     def get_first_playing_player(self):
@@ -105,7 +112,7 @@ class PlayerManager:
         current_player = self.get_first_playing_player()
         if current_player is not None:
             self.on_metadata_changed(current_player, current_player.props.metadata)
-        else:    
+        else:
             self.clear_output()
 
     def on_metadata_changed(self, player, metadata, _=None):
@@ -116,7 +123,11 @@ class PlayerManager:
         title = title.replace("&", "&amp;")
 
         track_info = ""
-        if player_name == "spotify" and "mpris:trackid" in metadata.keys() and ":ad:" in player.props.metadata["mpris:trackid"]:
+        if (
+            player_name == "spotify"
+            and "mpris:trackid" in metadata.keys()
+            and ":ad:" in player.props.metadata["mpris:trackid"]
+        ):
             track_info = "Advertisement"
         elif artist is not None and title is not None:
             track_info = f"{artist} - {title}"
@@ -130,26 +141,36 @@ class PlayerManager:
                 track_info = "P: " + track_info
         # only print output if no other player is playing
         current_playing = self.get_first_playing_player()
-        if current_playing is None or current_playing.props.player_name == player.props.player_name:
+        if (
+            current_playing is None
+            or current_playing.props.player_name == player.props.player_name
+        ):
             self.write_output(track_info, player)
         else:
-            logger.debug(f"Other player {current_playing.props.player_name} is playing, skipping")
+            logger.debug(
+                f"Other player {current_playing.props.player_name} is playing, skipping"
+            )
 
     def on_player_appeared(self, _, player):
         logger.info(f"Player has appeared: {player.name}")
         if player.name in self.excluded_player:
             logger.debug(
-                "New player appeared, but it's in exclude player list, skipping")
+                "New player appeared, but it's in exclude player list, skipping"
+            )
             return
-        if player is not None and (self.selected_player is None or player.name == self.selected_player):
+        if player is not None and (
+            self.selected_player is None or player.name == self.selected_player
+        ):
             self.init_player(player)
         else:
             logger.debug(
-                "New player appeared, but it's not the selected player, skipping")
+                "New player appeared, but it's not the selected player, skipping"
+            )
 
     def on_player_vanished(self, _, player):
         logger.info(f"Player {player.props.player_name} has vanished")
         self.show_most_important_player()
+
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
@@ -172,10 +193,14 @@ def main():
 
     # Initialize logging
     if arguments.enable_logging:
-        logfile = os.path.join(os.path.dirname(
-            os.path.realpath(__file__)), "media-player.log")
-        logging.basicConfig(filename=logfile, level=logging.DEBUG,
-                            format="%(asctime)s %(name)s %(levelname)s:%(lineno)d %(message)s")
+        logfile = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)), "media-player.log"
+        )
+        logging.basicConfig(
+            filename=logfile,
+            level=logging.DEBUG,
+            format="%(asctime)s %(name)s %(levelname)s:%(lineno)d %(message)s",
+        )
 
     # Logging is set by default to WARN and higher.
     # With every occurrence of -v it's lowered by one
