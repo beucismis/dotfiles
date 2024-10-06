a3f7482dbed582afb24efb9ae6481f848db01b19
beucismis <beucismis@tutamail.com>
2024-10-06 22:24:07 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 echo "Creating directories..."
-mkdir $HOME/screenshots $HOME/games $HOME/projects $HOME/clones $HOME/area51
-
-echo "Updating xdg user directory settings..."
-xdg-user-dirs-update
+mkdir /home/$USER/screenshots
+mkdir /home/$USER/games
+mkdir /home/$USER/projects
+mkdir /home/$USER/clones
+mkdir /home/$USER/area51
+mkdir /home/$USER/.local/bin 
 
 echo "Updating package list..."
 sudo pacman -Syu --noconfirm
@@ -15,14 +17,14 @@ if ! [ -x "$(command -v git)" ]; then
 fi
 
 echo "Cloning dotfiles repository from GitHub..."
-git clone https://github.com/beucismis/dotfiles $HOME/.dotfiles
+git clone https://github.com/beucismis/dotfiles /home/$USER/.dotfiles
 
 echo "Installing base-devel packages..."
 sudo pacman -S --needed base-devel
 
 echo "Installing paru..."
-git clone https://aur.archlinux.org/paru.git ~/.paru
-cd ~/.paru
+git clone https://aur.archlinux.org/paru-git.git /home/$USER/clones/.paru
+cd /home/$USER/clones/.paru
 makepkg -si
 
 if ! [ -x "$(command -v stow)" ]; then
@@ -31,15 +33,20 @@ if ! [ -x "$(command -v stow)" ]; then
 fi
 
 echo "Symlinking configuration files with stow..."
-cd $HOME/.dotfiles
+cd /home/$USER/.dotfiles
 sudo stow --adopt etc -t /etc/
 sudo stow --adopt usr -t /usr/
 stow --adopt bash fastfetch foot git heroic mako mangohud pip qt5ct qt6ct radiotray-ng sway waybar wob
 
+echo "Updating xdg user directory settings..."
+sudo pacman -S xdg-user-dirs --noconfirm
+xdg-user-dirs-update
+
 echo "Setting up Git completion and prompt..."
-cd $HOME/.local/bin
+cd /home/$USER/.local/bin
+rm -rf git-completion.bash* git-prompt.sh*
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
-chmod +rx $HOME/.local/bin/git-completion.bash $HOME/.local/bin/git-prompt.sh
+chmod +rx /home/$USER/.local/bin/git-completion.bash /home/$USER/.local/bin/git-prompt.sh
 
 echo "Done!"
