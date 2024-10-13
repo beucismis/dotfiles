7fafa8479b5c15773f9806c9519597e584703af3
beucismis <beucismis@tutamail.com>
2024-10-13 20:28:25 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-echo "Creating directories..."
-mkdir /home/$USER/screenshots
-mkdir /home/$USER/games
-mkdir /home/$USER/projects
-mkdir /home/$USER/clones
-mkdir /home/$USER/area51
-mkdir /home/$USER/.local/bin 
-
 echo "Updating package list..."
 sudo pacman -Syu --noconfirm
 
@@ -23,9 +15,8 @@ echo "Installing base-devel packages..."
 sudo pacman -S --needed base-devel
 
 echo "Installing paru..."
-git clone https://aur.archlinux.org/paru-git.git /home/$USER/clones/.paru
-cd /home/$USER/clones/.paru
-makepkg -si
+git clone https://aur.archlinux.org/paru-git.git /home/$USER/.paru
+cd /home/$USER/.paru && makepkg -si
 
 if ! [ -x "$(command -v stow)" ]; then
     echo "Installing stow package..."
@@ -36,17 +27,24 @@ echo "Symlinking configuration files with stow..."
 cd /home/$USER/.dotfiles
 sudo stow --adopt etc -t /etc/
 sudo stow --adopt usr -t /usr/
-stow --adopt bash fastfetch foot git heroic mako mangohud pip qt5ct qt6ct radiotray-ng sway waybar wob
+stow --adopt bash fastfetch foot git heroic mako mangohud pip qt5ct qt6ct radiotray-ng sway user-dirs waybar wob
 
 echo "Updating xdg user directory settings..."
 sudo pacman -S xdg-user-dirs --noconfirm
 xdg-user-dirs-update
 
+echo "Creating custom directories..."
+mkdir /home/$USER/screenshots
+mkdir /home/$USER/games
+mkdir /home/$USER/projects
+mkdir /home/$USER/clones
+mkdir /home/$USER/area51
+mkdir /home/$USER/.local/bin 
+
 echo "Setting up Git completion and prompt..."
-cd /home/$USER/.local/bin
-rm -rf git-completion.bash* git-prompt.sh*
-wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
-wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+rm -rf /home/$USER/.local/bin/git-completion.bash* /home/$USER/.local/bin/git-prompt.sh*
+wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -P /home/$USER/.local/bin
+wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -P /home/$USER/.local/bin
 chmod +rx /home/$USER/.local/bin/git-completion.bash /home/$USER/.local/bin/git-prompt.sh
 
 echo "Done!"
