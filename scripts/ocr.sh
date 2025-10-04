94d67fc0ac8a97c815f9cb9decca2302e8b05463
beucismis <beucismis@tutamail.com>
2025-10-04 10:26:18 +0300

    Add ocr.sh

---
+++ scripts/ocr.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+IMG_FILE=$(mktemp --suffix=.png)
+
+REGION=$(slurp) || exit 1
+grim -g "$REGION" "$IMG_FILE" || exit 1
+
+TEXT=$(tesseract "$IMG_FILE" stdout)
+
+rm "$IMG_FILE"
+
+echo "$TEXT" | wl-copy
+
+yad --title="OCR Result" \
+    --width=500 --height=300 \
+    --text-info --center \
+    --wrap --fontname="monospace 10" \
+    --button="Close":0 \
+    --filename=<(echo "$TEXT")
