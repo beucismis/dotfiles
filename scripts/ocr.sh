f4354508258edfa36747009b1320f4cba79bab6e
beucismis <beucismis@tutamail.com>
2026-02-15 12:45:45 +0300

    Update ocr.sh

--- scripts/ocr.sh
+++ scripts/ocr.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-
 IMG_FILE=$(mktemp --suffix=.png)
-
 REGION=$(slurp) || exit 1
 grim -g "$REGION" "$IMG_FILE" || exit 1
-
 TEXT=$(tesseract "$IMG_FILE" stdout)
-
 rm "$IMG_FILE"
-
 echo "$TEXT" | wl-copy
 
 yad --title="OCR Result" \
