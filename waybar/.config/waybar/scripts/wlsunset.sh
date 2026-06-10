350f614f76dd997d92892e5fc06aa07f14fa9df4
beucismis <beucismis@tutamail.com>
2026-06-10 11:27:33 +0300

    Update waybar

--- waybar/.config/waybar/scripts/wlsunset.sh
+++ waybar/.config/waybar/scripts/wlsunset.sh
@@ -8,7 +8,7 @@ if pgrep -x "wlsunset" > /dev/null; then
     fi
 else
     if [[ "$1" == "toggle" ]]; then
-        wlsunset -l 37.77426 -L 29.0876 &
+        wlsunset -S 19:30 -s 06:00 -t 4000 -T 6500 &
     else
         echo '{"text": "[LF: OFF]", "class": "deactivated"}'
     fi
